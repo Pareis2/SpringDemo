f5a0309995b1461d7afca3b16a51d7e0f0b64052
Pareis Pindoria <pareis.pindoria@justice.gov.uk>
2018-08-29 16:08:04 +0100

    prime numbers exercise

--- spring.demo 2/src/main/java/pareis/spring/example/spring/demo/FirstController.java
+++ spring.demo 2/src/main/java/pareis/spring/example/spring/demo/FirstController.java
@@ -13,9 +13,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstController {
     @RequestMapping(method = RequestMethod.GET)
-    public String index() {
-            return "Hello";
+    //public String index() { return "Hello"; }
+
+    public static void main(final String[] args) {
+        System.out.print("Enter a number: ");
+        final Scanner key = new Scanner(System.in);
+        final int in = key.nextInt();
+
+        if (isPrime(in)) {
+            System.out.println(in + " is a prime number");
+        } else {
+            System.out.println(in + " is not a prime number");
+        }
     }
+
+    private static boolean isPrime(final int in) {
+        if (in < 2) return false;
+
+        for (int i=2; i <= Math.sqrt(in); i++){
+            if (in%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+
 }
 
 
